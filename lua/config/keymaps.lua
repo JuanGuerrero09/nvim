1caf52fde636add5779d6bf1ebfc1a89ee07384c
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2025-02-28 14:18:19 +0100

    added keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<CR>", "o<Esc>", { desc = "Insert new line below and stay in normal mode" })
+vim.keymap.set("n", "<S-CR>", "O<Esc>", { desc = "Insert new line above and stay in normal mode" })
