792b1de67c80138125eb6a79ff6496bfc50fb0a3
juanguerrero09 <juanguerrero09mc@gmail.com>
2025-03-21 22:40:51 +0100

    updated keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,4 @@
 
 vim.keymap.set("n", "<CR>", "o<Esc>", { desc = "Insert new line below and stay in normal mode" })
 vim.keymap.set("n", "<S-CR>", "O<Esc>", { desc = "Insert new line above and stay in normal mode" })
+vim.keymap.set("i", "<S-{>", "{<CR>}<Esc>O", { desc = "Add new line and enter inside {}" })
